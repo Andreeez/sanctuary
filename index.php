7dbabdd4c321fb97dfd88cf999e7827f37aceae7
Andreeez <32762098+Andreeez@users.noreply.github.com>
2018-02-07 14:45:54 +0100

    form klar

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
-<?php
-    echo "Hello World!";
-?>
+<html>
+    <head>
+    <head>
+    <body>
+        <form method="POST" action="/results.php">
+            <label>Tiger</label>
+            <input type="number" name="numberAnimal">
+            <label>Giraff</label>
+            <input type="number" name="numberAnimal">
+            <label>Apor</label>
+            <input type="number" name="numberAnimal">
+            <button type="submit">Skicka iväg möget</button>
+
+        </form>
+    </body>
+</html>
