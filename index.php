da6be6e95497bf1f3e619719b3dec9a7dde96869
Andreeez <32762098+Andreeez@users.noreply.github.com>
2018-02-08 14:41:10 +0100

    loop funkar

--- index.php
+++ index.php
@@ -2,13 +2,15 @@
     <head>
     <head>
     <body>
-        <form method="POST" action="/results.php">
+        <form method="GET" action="/results.php">
             <label>Tiger</label>
-            <input type="number" name="numberAnimal">
+            <input type="number" name="numberTiger">
             <label>Giraff</label>
-            <input type="number" name="numberAnimal">
+            <input type="number" name="numberGiraff">
             <label>Apor</label>
-            <input type="number" name="numberAnimal">
+            <input type="number" name="numberApor">
+            <label>Kokosnöt</label>
+            <input type="number" name="numberKokosNut">
             <button type="submit">Skicka iväg möget</button>
         </form>
     </body>
--- results.php
+++ results.php
@@ -2,60 +2,106 @@
 
     abstract class SanctuaryFarm {
         protected $image;
+        protected $sound;               
 
-            public abstract function onClick();
+            private function onClick(){                
+                $names = array("Pelle","Pele","Kajsa","jenny","Amanda","Joakim","Jonathan","Anton","Thompa","Yeppsson","krasse","pedram","faroch","louise","Daniel","Emil");
+                $this->allName = $names[array_rand($names)];
+                return $this->onClickSpecific();
+            }
+
+            public abstract function onClickSpecific();
 
             public function draw(){
-            
+          
             echo "<img style='width: 300px; height: 300px;' src='/".$this->image."' onclick='".$this->onClick()."'/>";
-            // echo "<img style='margin: 100px;' src='/jsFromPhp/".$this->image."' onclick='".$this->onClick()."' />";
     }
 }
 
 class ApaCharacter extends SanctuaryFarm {
     protected $image = 'apa.jpg';
+    protected $sound = 'Ohohohohohoho';
+    
+    public function onClickSpecific(){
+        return "alert(\" $this->sound + $this->allName\");";
 
-    public function onClick(){
-        return "alert(\"Ohohohohohoho\");";
     }
 }
 
 class GiraffCharacter extends SanctuaryFarm {
+    protected $sound = 'Wihihihi';
     protected $image = 'giraff.jpg';
 
-    public function onClick(){
-        return "alert(\"Gitigiaihia\");";
+    public function onClickSpecific(){
+        return "alert(\" $this->sound + $this->allName\");";
+
     }
 }
 
 class TigerCharacter extends SanctuaryFarm{
     protected $image ='tiger.jpg';
+    protected $sound = 'WraowWraow';
 
-    public function onClick(){
-        return "alert(\"WraowWraow\");";
+    public function onClickSpecific(){
+        return "alert(\" $this->sound + $this->allName\");";
+
     }
 }
 
 class KokosNotCharacter extends SanctuaryFarm {
     protected $image = 'kokosnot.jpg';
 
-    public function onClick(){
+    public function onClickSpecific(){
         return null;
     }
 }
 
+// $farm = array(
+//     new ApaCharacter(),
+//     new KokosNotCharacter(),
+//     new GiraffCharacter(),
+//     new TigerCharacter()
 
-$farm = array(
-    new ApaCharacter(),
-    new KokosNotCharacter(),
-    new GiraffCharacter(),
-    new TigerCharacter()
+// );
 
-);
+$tiger = $_GET['numberTiger'];
+$giraff = $_GET['numberGiraff'];
+$apor = $_GET['numberApor'];
+$kokosnot = $_GET['numberKokosNut'];
 
-foreach ($farm as $member){
-    $member->draw();
+for($i = 0; $i < $tiger; $i++ ){
+    $tigern = new TigerCharacter();
+    $tigern->draw();
 }
+
+for($i = 0; $i < $giraff; $i++ ){
+    $giraffn = new GiraffCharacter();
+    $giraffn->draw();
+}
+
+for($i = 0; $i < $apor; $i++ ){
+    $aporn = new ApaCharacter();
+    $aporn->draw();
+}
+
+for($i = 0; $i < $kokosnot; $i++ ){
+    $kokosnotn = new KokosNotCharacter();
+    $kokosnotn->draw();
+}
+
+// foreach ($farm as $member){
+//     $member->draw();
+// }
+//         echo "What was gotten: <br/>";
+//         print_r($_GET ['numberTiger']);
+//         echo "<br/>";
+//         print_r($_GET ['numberGiraff']);
+//         echo "<br/>";
+//         print_r($_GET ['numberApor']);
+//         echo "<br/>";
+//         print_r($_GET ['numberKokosNut']);
+        
+        // print_r( $_GET ['numberTiger', 'numberGiraff' , 'numberApor'] );
 
 ?>
 
